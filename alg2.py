e5d210fd265eb84271f060712394a195c4a4fdf3
IvanIK1 <kurakov_2016@bk.ru>
2026-04-01 14:40:51 +0300

    Алгоритм перевода в двочную систему

---
+++ alg2.py
@@ -0,0 +1,18 @@
+def count_bits(n):
+    N = n 
+    bin_n = format(N , "b")
+    bin_count = bin_n.count('1')
+    return bin_count, bin_n, N
+bin_count, bin_n, N = count_bits(n = 1234)
+# print(f'Число: {N}\nДвочиный формат числа {N}:{bin_n}\nКоличество едbниц в двоичном формате числа {N}:{bin_count}')
+
+def To_bin(n):
+    n 
+    bin = ""
+    while n > 0:
+        ost = n % 2
+        bin= str(ost) + bin  # добавляем остаток в начало
+        n = n // 2  # делим на 2 (целочисленное деление)
+    return bin
+
+print(To_bin(504))
